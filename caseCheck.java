c896816aa0d25b5403660d0b27099e31ffab6cd3
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-20 21:30:37 +0530

    Update caseCheck.java

--- caseCheck.java
+++ caseCheck.java
@@ -5,12 +5,15 @@ public class caseCheck {
         Scanner in = new Scanner(System.in);
         char ch=in.next().trim().charAt(0);
 //        String word = "hello";
+        
 //        //System.out.println(in.next().trim());
+        
         if (ch>= 'a' && ch <= 'z'){
             System.out.println("Lower latter");
         }else{
             
             //Messages
+            
             System.out.println("Upper case");
         }
         System.out.println();
