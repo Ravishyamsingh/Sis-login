8facb0a65e22b1abc69e4bbbdc58b938daf68f76
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-30 21:16:54 +0530

    Update caseCheck.java

--- caseCheck.java
+++ caseCheck.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+\import java.util.Scanner;
 
 public class caseCheck {
     public static void main(String[] args) {
@@ -20,5 +20,6 @@ public class caseCheck {
 
 
 
+ 
     }
 }
