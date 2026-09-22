9d0307ac3557b93dc8a2e2b2dbe74f1a57b26d96
rss coder <ravishyamsingh52325@gmail.com>
2026-09-22 11:52:34 +0530

    Update Basic.java

--- Basic.java
+++ Basic.java
@@ -2,6 +2,9 @@ public class Basic {
     public static void main(String[] args) {
 //        int a=10;
 //        if(a==10){
+
+
+        
 //            System.out.println("Hello world");
 //        }
 
